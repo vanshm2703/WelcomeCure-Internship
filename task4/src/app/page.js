370c6e8f9c38dda3a002613d136da4d57df34d81
vanshm2703 <vanshm2703@gmail.com>
2024-07-09 12:29:00 +0530

    created a login home page and on login redirected to dashboard page

--- task4/src/app/page.js
+++ task4/src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Dummy authentication logic
+    // Dummy authentication logics
     if (email === 'name@example.com' && password === 'password') {
       router.push('/dashboard');
     } else {
